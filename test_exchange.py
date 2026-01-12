93e7ca9c37f360f9f7aaf46fe55188e897d2e3f2
Samuel Ius <s.ius@outlook.com>
2026-01-11 20:13:55 -0500

    Add Exchange class with tests

---
+++ test_exchange.py
@@ -0,0 +1,15 @@
+from exchange import Exchange
+
+def test_exchange_creation():
+    uniswap = Exchange("Uniswap")
+    assert uniswap.name == "Uniswap"
+    assert len(uniswap.prices) == 0
+
+def test_add_price():
+    uniswap = Exchange("Uniswap")
+    uniswap.add_price("Bitcoin", 50000)
+    assert uniswap.get_price("Bitcoin") == 50000
+
+def test_get_price_missing_coin():
+    uniswap = Exchange("Uniswap")
+    assert uniswap.get_price("Dogecoin") == 0
